f646efa8538d218f8a7c0e929d0260045241544b
William Fernandes Dias <william.winchester1967@gmail.com>
2024-05-08 18:34:56 -0300

    test: add tests for crawler options processing

---
+++ tests/test_options.py
@@ -0,0 +1,11 @@
+from naja.options import DEFAULT_OPTIONS, merge_with_default_options
+
+
+class DescribeMergeWithDefaultOptions:
+    def it_adds_default_options(self):
+        options = merge_with_default_options({"workers": 5})
+        assert options == {**DEFAULT_OPTIONS, "workers": 5}
+
+    def it_returns_default_options_if_none(self):
+        options = merge_with_default_options()
+        assert options == DEFAULT_OPTIONS
