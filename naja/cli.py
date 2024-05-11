a5e5c86eee59bb9fc3e75fa85519cce62638e2e7
William Fernandes Dias <william.winchester1967@gmail.com>
2024-05-10 21:27:29 -0300

    feat: implement simple command line interface to the crawler

--- naja/cli.py
+++ naja/cli.py
@@ -1,17 +1,75 @@
 """Console script for naja."""
 
+import asyncio
 import sys
+from typing import Callable, List, Union
 
 import click
 
+from naja.crawler import Crawler
+from naja.options import CrawlerOptions
 
-@click.command()
-def main() -> int:
-    """Console script for naja."""
-    click.echo("Replace this message by putting your code into " "naja.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
+
+def cli_for(
+    crawler_factory: Callable[[List[str], CrawlerOptions], Crawler]
+) -> Callable[..., int]:
+    """Return a CLI function for the given crawler factory.
+
+    Args:
+        crawler_factory (Callable[[List[str], CrawlerOptions], Crawler]): A factory
+        function that returns a Crawler instance.
+
+    Returns:
+        Callable[..., int]: A CLI function.
+    """
+
+    @click.command("naja")
+    @click.argument("urls", required=True, type=str, nargs=-1)
+    @click.option(
+        "--workers",
+        "-w",
+        type=int,
+        default=5,
+        show_default=True,
+        help="Number of workers to use.",
+    )
+    @click.option(
+        "--limit",
+        "-l",
+        type=int,
+        default=20,
+        show_default=True,
+        help="Maximum number of URLs to crawl.",
+    )
+    @click.option(
+        "--agent",
+        "-u",
+        type=str,
+        default="naja",
+        show_default=True,
+        help="User agent to use for the requests.",
+    )
+    def main(*urls: str, **kwargs: Union[int, str]) -> int:
+        """Console script for naja."""
+        crawler = crawler_factory(
+            list(urls),
+            {
+                "workers": int(kwargs["workers"]),
+                "limit": int(kwargs["limit"]),
+                "user_agent": str(kwargs["agent"]),
+            },
+        )
+        asyncio.run(crawler.run())
+        click.secho(f"Visited {len(crawler.urls_seen)} URLs:", fg="green")
+        for url in crawler.urls_seen:
+            click.echo(url)
+        return 0
+
+    return main
+
+
+main = cli_for(Crawler)
 
 
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    sys.exit(main())
